3adfb09f7342b71aed43e15a7b0c8d946d0c9241
Ryan Rhodes <rrhodes.webdev@gmail.com>
2018-04-27 18:49:09 -0400

    Added Paper to Sign Views
    added material paper to form sign up and sign in views

--- src/components/SignUpForm.js
+++ src/components/SignUpForm.js
@@ -1,35 +1,40 @@
 import React from 'react';
 import TextField from 'material-ui/TextField';
 import RaisedButton from 'material-ui/RaisedButton';
-import FontIcon from 'material-ui/FontIcon';
+import Paper from 'material-ui/Paper';
 
 function SignUpForm() {
   return(
     <div>
-      <TextField 
-        floatingLabelText='Username'
-      />
-        <br />
-      <TextField 
-      floatingLabelText='Email'
-      type='email'
-      />
-        <br />
-      <TextField 
-      floatingLabelText="Password"
-      type="password"
-      />
-        <br />
-      <RaisedButton
-        label='Sign Up'
-      />
-        <br />
-      <RaisedButton 
-        label='Facebook'
-      />
-      <RaisedButton 
-        label='Google'
-      />
+      <Paper className='form-paper' zDepth={2}>
+        <TextField 
+          floatingLabelText='Username'
+        />
+          <br />
+        <TextField 
+        floatingLabelText='Email'
+        type='email'
+        />
+          <br />
+        <TextField 
+        floatingLabelText="Password"
+        type="password"
+        />
+          <br />
+        <RaisedButton
+          label='Sign Up'
+          className='sign-btn'
+        />
+          <br />
+        <RaisedButton 
+          label='Facebook'
+          className='social-sign-btn'
+        />
+        <RaisedButton 
+          label='Google'
+          className='social-sign-btn'
+        />
+      </Paper>
     </div>
   )
 }
