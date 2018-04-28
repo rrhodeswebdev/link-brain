e4074a150d8a60503a4693a9b3f6564d8af138bb
Ryan Rhodes <rrhodes.webdev@gmail.com>
2018-04-28 12:58:54 -0400

    Campaign Static View
    renders a list of campaigns in the campaign view

--- src/components/CampaignListItem.js
+++ src/components/CampaignListItem.js
@@ -2,15 +2,34 @@ import React from 'react';
 import { Card, CardTitle, CardText } from 'material-ui/Card';
 
 function CampaignListItem() {
+
   return(
-    <div>
-      <Card>
+    <div className='campaign-boxes-list'>
+      <Card className='campaign-card'>
         <CardTitle 
           title='Campaign 1'
           subtitle='Blog Post'
         />
         <CardText>
-          <span>Created on: Date</span><span>Number of Opportunities: #</span>
+          <span>Created on: Date</span>
+        </CardText>
+      </Card>
+      <Card className='campaign-card'>
+        <CardTitle 
+          title='Campaign 2'
+          subtitle='Blog Post'
+        />
+        <CardText>
+          <span>Created on: Date</span>
+        </CardText>
+      </Card>
+      <Card className='campaign-card'>
+        <CardTitle 
+          title='Campaign 3'
+          subtitle='Blog Post'
+        />
+        <CardText>
+          <span>Created on: Date</span>
         </CardText>
       </Card>
     </div>
