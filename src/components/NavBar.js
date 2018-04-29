02d91ccbd83357e5ec0d0c58206f173f102009b0
Ryan Rhodes <rrhodes.webdev@gmail.com>
2018-04-29 18:49:44 -0400

    Final Static Updates
    updated final static version of components

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,27 +1,28 @@
 import React from 'react';
 import AppBar from 'material-ui/AppBar';
-import FlatButton from 'material-ui/FlatButton';
 import Paper from 'material-ui/Paper';
 import Menu from 'material-ui/Menu';
 import MenuItem from 'material-ui/MenuItem';
 import FontIcon from 'material-ui/FontIcon';
 import { Link } from 'react-router-dom';
+import MoreVertIcon from 'material-ui/svg-icons/navigation/more-vert';
+import IconMenu from 'material-ui/IconMenu';
+import IconButton from 'material-ui/IconButton';
 
 function NavBar() {
-  const rightButtons = (
-    <div>
-      <Link to='/sign-up'><FlatButton label="Sign Up" /></Link>
-      <Link to='/sign-in'><FlatButton label="Sign In" /></Link>
-    </div>
-  );
-
   return(
     <div>
-      <AppBar 
-        title='LinkBrain'
-        showMenuIconButton = {false}
-        iconElementRight = {rightButtons}
-      />
+      <AppBar title='LinkBrain' showMenuIconButton={false} style={{backgroundColor: '#42A5F5'}}>
+        <IconMenu
+          iconButtonElement={<IconButton><MoreVertIcon color='#FAFAFA'/></IconButton>}
+          anchorOrigin={{horizontal: 'right', vertical: 'top'}}
+          targetOrigin={{horizontal: 'right', vertical: 'top'}}
+          style={{marginTop: '8px'}}
+        >
+          <Link to='/sign-in' style={{textDecoration: 'none'}}><MenuItem primaryText='Sign In' /></Link>
+          <Link to='/sign-up' style={{textDecoration: 'none'}}><MenuItem primaryText='Sign Up' /></Link>
+        </IconMenu>
+      </AppBar>
       <Paper className='left-menu' zDepth={2}>
         <Menu>
           <Link to='/dashboard' style={{textDecoration: 'none'}}><MenuItem primaryText='Home' leftIcon={<FontIcon className='material-icons'>home</FontIcon>} /></Link>
