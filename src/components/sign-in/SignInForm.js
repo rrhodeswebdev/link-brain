d91778d7d1eeb993d9e9584efece57249b5a4f52
Ryan Rhodes <rrhodes.webdev@gmail.com>
2018-06-03 13:03:24 -0400

    Auth Components
    sign in and sign up pages styled and front end validation added

--- src/components/sign-in/SignInForm.js
+++ src/components/sign-in/SignInForm.js
@@ -3,60 +3,103 @@ import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
 import { Field, reduxForm } from 'redux-form';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
 import './sign-in.css'
 
 const renderTextField = ({
   input,
   label,
+  meta: { touched, error }
 }) => (
   <TextField
+    margin='normal'
+    style={{width: '100%'}}
     label={label}
     type='string'
+    required
+    helperText={touched && error}
     {...input}
   />
-)
+);
 
 const renderPassword = ({
   input,
   label,
+  meta: { touched, error }
 }) => (
   <TextField
-    label={label}
-    type='password'
-    {...input}
+  margin='normal'
+  style={{width: '100%'}}
+  label={label}
+  type='password'
+  required
+  helperText={touched && error}
+  {...input}
   />
-)
+);
+
+const validate = values => {
+  const errors = {};
+  const requiredFields = [ 'username', 'email', 'password' ]
+
+  requiredFields.forEach(field => {
+    if(!values[ field ]) {
+      errors[ field ] = 'Required'
+    }
+  })
+  if(values.email && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address'
+  }
+  return errors
+}
 
 class SignInForm extends Component {
   render() {
     const submit = this.props.handleSubmit(this.props.onSubmit);
+
     return(
-      <Paper className = 'sign-in-form'>
-        <Field
-          name='username'
-          label='Username'
-          component={renderTextField}
-        />
-        <Field
-          name='password'
-          label='Password'
-          component={renderPassword}
-        />
-          <Button variant="raised" color="primary" className='sign-in-btn' onClick={() => {submit()}}>
-            Sign In
-          </Button>
-          <p><em>Don't have an account? Sign up <a href='/sign-up'>here</a></em></p>
-          <Button variant="raised" color="secondary">
-            Facebook
-          </Button>
-          <Button variant="raised" color="secondary">
-            Google
-          </Button>
-      </Paper>
+      <Grid container justify='center' alignItems='center' style={{height: '100vh', backgroundColor: '#ecf0f1'}}>
+        <Grid item>
+          <Paper className = 'sign-in-form'>
+            <Grid item>
+              <Typography align='center' variant='display1' style={{padding: '20px 0'}}>
+                Sign In
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Field
+                name='username'
+                label='Username'
+                component={renderTextField}
+              />
+            </Grid>
+            <Grid item>
+              <Field
+                name='password'
+                label='Password'
+                component={renderPassword}
+              />
+            </Grid>
+            <Grid item>
+              <Button variant="raised" color="primary" onClick={() => {submit()}} style={{margin: '40px 0', width: '100%'}}>
+                Sign In
+              </Button>
+            </Grid>
+            <Grid item>
+              <Typography align='center' variant='subheading'>
+                  Need an account? Sign up <a href='/sign-up'>here</a>
+              </Typography>
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
     )
   }
 }
 
 export default reduxForm({
-  form: 'SignInForm'
+  form: 'SignInForm',
+  validate
 })(SignInForm);
--- src/components/sign-up/SignUpForm.js
+++ src/components/sign-up/SignUpForm.js
@@ -3,15 +3,23 @@ import { Field, reduxForm } from 'redux-form';
 import Paper from '@material-ui/core/Paper';
 import TextField from '@material-ui/core/TextField';
 import Button from '@material-ui/core/Button';
+import Grid from '@material-ui/core/Grid';
+import Typography from '@material-ui/core/Typography';
+
 import './sign-up.css';
 
 const renderTextField = ({
   input,
   label,
+  meta: { touched, error }
 }) => (
   <TextField
+    margin='normal'
+    style={{width: '100%'}}
     label={label}
     type='string'
+    required
+    helperText={touched && error}
     {...input}
   />
 );
@@ -19,50 +27,86 @@ const renderTextField = ({
 const renderPassword = ({
   input,
   label,
+  meta: { touched, error }
 }) => (
   <TextField
+    margin='normal'
+    style={{width: '100%'}}
     label={label}
     type='password'
+    required
+    helperText={touched && error}
     {...input}
   />
 );
+
+const validate = values => {
+  const errors = {};
+  const requiredFields = [ 'username', 'email', 'password' ]
+
+  requiredFields.forEach(field => {
+    if(!values[ field ]) {
+      errors[ field ] = 'Required'
+    }
+  })
+  if(values.email && !/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,4}$/i.test(values.email)) {
+    errors.email = 'Invalid email address'
+  }
+  return errors
+}
 
 class SignUpForm extends Component {
   render() {
     const submit = this.props.handleSubmit(this.props.onSubmit);
     
     return(
-      <Paper className = 'sign-up-form'>
-        <Field 
-            name='username'
-            label='Username'
-            component={renderTextField}
-          />
-        <Field 
-            name='email'
-            label='Email'
-            component={renderTextField}
-        />
-        <Field 
-            name='password'
-            label='Password'
-            component={renderPassword}
-        />
-        <Button onClick={() => {submit()}} variant="raised" color="primary" className='sign-up-btn'>
-          Sign Up
-        </Button>
-        <p><em>Already have an account? Sign in <a href='/sign-in'>here</a></em></p>
-        <Button variant="raised" color="secondary">
-          Facebook
-        </Button>
-        <Button variant="raised" color="secondary">
-          Google
-        </Button>
-      </Paper>
+      <Grid container justify='center' alignItems='center' style={{height: '100vh', backgroundColor: '#ecf0f1'}}>
+        <Grid item>
+          <Paper className = 'sign-up-form'>
+            <Grid item>
+              <Typography align='center' variant='display1' style={{padding: '20px 0'}}>
+                Create an Account
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Field 
+                  name='username'
+                  label='Username'
+                  component={renderTextField}
+                />
+            </Grid>
+            <Grid item>
+              <Field 
+                  name='email'
+                  label='Email'
+                  component={renderTextField}
+              />
+            </Grid>
+            <Grid item>
+              <Field 
+                  name='password'
+                  label='Password'
+                  component={renderPassword}
+              />
+            </Grid>
+            <Grid item>
+              <Button onClick={() => {submit()}} variant="raised" color="primary" style={{margin: '40px 0', width: '100%'}}>
+                Sign Up
+              </Button>
+            </Grid>
+            <Grid item>
+              <Typography align='center' variant='subheading'>
+                Already have an account? Sign in <a href='/sign-in'>here</a>
+              </Typography>
+            </Grid>
+          </Paper>
+        </Grid>
+      </Grid>
     )
   }
 }
 
 export default reduxForm({
-  form: 'SignUpForm'
+  form: 'SignUpForm', 
+  validate
 })(SignUpForm);
