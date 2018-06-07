f313e2a44492e03e1d91cefdf92903002787055a
Ryan Rhodes <rrhodes.webdev@gmail.com>
2018-06-06 20:56:44 -0400

    data chart
    updated dashboard charts

--- src/components/dashboard/DataChart.js
+++ src/components/dashboard/DataChart.js
@@ -1,15 +1,15 @@
-import React from 'react';
-import moment from 'moment';
-import { LineChart, Line, XAxis, YAxis, CartesianGrid } from 'recharts';
+import React from "react";
+import moment from "moment";
+import { LineChart, Line, XAxis, YAxis, CartesianGrid } from "recharts";
 
 function DataChart(props) {
   const data = props.data;
-  const week = moment().subtract(7, 'd');
+  const week = moment().subtract(7, "d");
   const weekData = [];
   const newData = [];
 
   data.map(item => {
-    if(moment(item.updated) > week) {
+    if (moment(item.updated) > week) {
       return weekData.push(item);
     }
     return weekData;
@@ -18,29 +18,29 @@ function DataChart(props) {
   let count = 0;
   let day;
 
-  weekData.forEach((data) => {
-    if(moment(data.updated).isSame(data.updated, 'day')) {
+  weekData.forEach(data => {
+    if (moment(data.updated).isSame(data.updated, "day")) {
       count++;
-      day = moment(data).format('ddd');
+      day = moment(data).format("ddd");
     }
     console.log(count);
     console.log(day);
     newData.push({
-      day: moment(data).format('ddd'),
+      day: moment(data).format("ddd"),
       count: count
-    })
+    });
   });
 
   console.log(newData);
 
-  return(
+  return (
     <LineChart data={newData} width={500} height={200}>
-      <XAxis dataKey='day'/>
+      <XAxis dataKey="day" />
       <YAxis />
-      <CartesianGrid strokeDasharray='3 3'/>
-      <Line type="monotone" dataKey='count' stroke='#8884d8' fill='#8884d8'/>
+      <CartesianGrid strokeDasharray="3 3" />
+      <Line type="monotone" dataKey="count" stroke="#8884d8" fill="#8884d8" />
     </LineChart>
-  )
+  );
 }
 
 export default DataChart;
